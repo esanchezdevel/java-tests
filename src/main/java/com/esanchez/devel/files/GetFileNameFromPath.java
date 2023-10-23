0ba34005f51bee6b879e5fe549d0a11754838d15
Enrique Sanchez <enrique.sanchez@hpe.com>
2023-10-23 13:55:22 +0200

    feat: add new example of GetFileNameFromPath, and add a comment explaining what we are testing

--- src/main/java/com/esanchez/devel/files/GetFileNameFromPath.java
+++ src/main/java/com/esanchez/devel/files/GetFileNameFromPath.java
@@ -4,6 +4,10 @@ import org.apache.commons.io.FilenameUtils;
 
 public class GetFileNameFromPath {
 
+	/*
+	 * In this class we can see some examples of which values we will receive 
+	 * when we execute the FilenameUtils.getName() method over different paths
+	 */
 	public static void main(String[] args) {
 		
 		String path1 = "/temp/path/file.xml";
@@ -25,5 +29,10 @@ public class GetFileNameFromPath {
 		String fileName4 = FilenameUtils.getName(path4);
 		
 		System.out.println("filename 4: " + fileName4);
+		
+		String path5 = "uuid?path=file.txt";
+		String fileName5 = FilenameUtils.getName(path5);
+		
+		System.out.println("filename 5: " + fileName5);
 	} 
 }
